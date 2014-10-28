a74a3ef7d5d96c1d97c79fb686a18c037d04cae7
Micky Hulse <mickyhulse@gmail.com>
2014-10-27 18:26:43 -0700

    Lane county.

--- source/files/scripts/pending.js
+++ source/files/scripts/pending.js
@@ -176,6 +176,75 @@
 	});
 	
 	$.pollster({
+		target: 'results-lane-county',
+		api: 'http://projects.registerguard.com/ballot/json/laneco/',
+		callback: function($data, $options) {
+			
+			var $this = $(this);
+			var template;
+			
+			$.each($data, function(i1, v1) {
+				
+				var $contest;
+				var id1 = 'con-' + v1.contest_id;
+				
+				if ( ! $options.flag) {
+					
+					template = [
+						'<div id="' + id1 + '" class="poll">',
+							'<div class="poll-spinner"></div>',
+							'<h6 class="poll-head">' + v1.contest_wrapper + '</h6>',
+							'<div class="poll-content">',
+							'</div>',
+						'</div>'
+					].join('\n');
+					
+					$this.append(template);
+					
+				}
+				
+				$.each(v1.options, function(i2, v2) {
+					
+					var id2 = 'con-' + v1.contest_id + '_cand-meas-' + v2.cand_meas_id;
+					var percent = v2.percent_of_lane_votes + '%';
+					
+					if ( ! $options.flag) {
+						
+						template = [
+							'<div class="poll-bar">',
+								'<span class="poll-bar-title">' + v2.name + '</span>',
+								'<div class="poll-grid">',
+									'<div class="poll-grid-secondary">',
+										'<span class="poll-bar-count">' + v2.lane_votes + '</span>',
+									'</div>',
+									'<div class="poll-grid-primary">',
+										'<div class="poll-bar-wrap">',
+											'<div id="' + id2 + '" class="poll-bar-votes">',
+												'<span></sapn>',
+											'</div>',
+										'</div>',
+									'</div>',
+								'</div>',
+							'</div>'
+						].join('\n');
+						
+						$('#' + id1 + ' .poll-content').append(template);
+						
+					}
+					
+					$('#' + id2)
+						.width(percent)
+						.children('span')
+						.text(percent);
+					
+				});
+				
+			});
+			
+		}
+	});
+	
+	$.pollster({
 		target: 'results-eugspr',
 		api: 'http://projects.registerguard.com/ballot/json/eugspr/',
 		callback: function($data, $options) {
